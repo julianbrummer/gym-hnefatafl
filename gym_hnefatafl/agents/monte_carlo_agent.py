7649e4f399625ab93aa14d7127dac45f6ccbcc6e
Julian <jbrummer@students.uni-mainz.de>
2018-11-25 13:37:33 +0100

    started with implementation 3.2

--- gym_hnefatafl/agents/monte_carlo_agent.py
+++ gym_hnefatafl/agents/monte_carlo_agent.py
@@ -1,8 +1,11 @@
 import copy
+import random
+import numpy as np
 from gym_hnefatafl.envs import HnefataflEnv
 from gym_hnefatafl.envs.board import Outcome, Player
 
 MONTE_CARLO_ITERATIONS = 1000
+MIN_NUM_VISITS_INTERNAL = 5 # may have to be much higher go uses 9*9
 
 #################################################################################################################
 #################################################################################################################
@@ -22,12 +25,14 @@ class Tree(object):
     # board: the current board
     # player: the player that this agent represents
     def __init__(self, board, player):
-        self.root = InternalNode(player)
-        self.state = board
+        self.root = Node(player)
+        self.board = board
+        self.num_simulations = 0
 
     # simulates an entire game
     def simulate_game(self):
         # init
+        self.num_simulations += 1
         # game history saves all actions done during this simulation
         game_history = []
         board_copy = copy.deepcopy(self.board)
@@ -35,7 +40,7 @@ class Tree(object):
 
         # simulate actions within the tree until we are no longer at an internal node
         while board_copy.outcome == Outcome.ongoing:
-            current_node, action = current_node.simulate_action(board_copy)
+            current_node, action = current_node.choose_and_simulate_action(board_copy)
             game_history.append(action)
             if current_node is None:
                 break
@@ -68,13 +73,17 @@ class Tree(object):
 
 
 # represents a node within the monte carlo search tree (that is actually stored in memory -> see the paper)
-class InternalNode(object):
+class Node(object):
     def __init__(self, player):
         self.children = {}
         self.number_of_visits = 0
         self.sum_of_values = 0
         self.sum_of_squared_values = 0
+        self.mean = 0.0
+        self.variance = 0.0
         self.player = player
+        self.sorted_child_indices = []
+        self.action_probabilities = []
 
     # chooses and simulates an action
     def choose_and_simulate_action(self, board):
@@ -83,7 +92,7 @@ class InternalNode(object):
         board.do_action(action, self.player)
         # create child node if this node has already been visited
         if self.number_of_visits > 1 and action not in self.children:
-            child_node = InternalNode(other_player(self.player))
+            child_node = Node(other_player(self.player))
             self.children[action] = child_node
             return child_node, action
         else:
@@ -91,12 +100,35 @@ class InternalNode(object):
 
     # chooses an action
     def __choose_action__(self, board):
-        raise NotImplementedError
+        if self.is_internal():  # do random move based on probability distribution
+            return np.random.choice(self.children, 1, self.action_probabilities)
+        else:  # do random move for external nodes
+            return random.choice(board.get_valid_actions(self.player))
 
     # updates the internal values
     def update_value(self, value):
         self.sum_of_values += value
         self.sum_of_squared_values += value * value
+
+    def update_action_probabilities(self):
+        # sort actions/children descending by mean value of children (or ascending by -mean see paper)
+        sorted_children = sorted(self.children.items(), key=lambda ac: ac[1].mean)
+        # compute probabilities for each move
+        self.action_probabilities = [Node.probability(sorted_children, i) for i in enumerate(sorted_children)]
+
+    def is_internal(self):
+        return self.number_of_visits > MIN_NUM_VISITS_INTERNAL
+
+    @staticmethod
+    def probability(children, i):
+        ai = 0  # just for now
+        epsi = (0.1 + pow(2, -i) + ai)/len(children)
+        m0 = children[0][1].mean
+        mi = children[i][1].mean
+        s0 = children[0][1].variance
+        si = children[i][1].variance
+        return np.exp(-2.4*(m0-mi)/np.sqrt(2*(s0*s0+si*si))) + epsi
+
 
 
 # returns the opponent of the given player
